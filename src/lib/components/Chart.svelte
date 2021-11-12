2b6ece3556b35d2ed8d5147bae9b2f9ccce6837d
Phillip Malboeuf <phil@phils.computer>
2021-11-12 11:10:02 -0500

    icon

--- src/lib/components/Chart.svelte
+++ src/lib/components/Chart.svelte
@@ -82,9 +82,14 @@
 </script>
 
 <figure class:small bind:this={element}></figure>
-{#if exporting}<button on:click={() => exporting.download('png')}>Export</button>{/if}
+<aside>
+  <a href="/indicateurs/{id}"><small>Plus d'info</small></a>
+  {#if exporting}<button on:click={() => exporting.download('png')} aria-label="Télécharger">
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-download"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="7 10 12 15 17 10"></polyline><line x1="12" y1="15" x2="12" y2="3"></line></svg>
+  </button>{/if}
+</aside>
 
-<style>
+<style lang="scss">
   figure {
     height: 50vh;
     margin: var(--gutter) 0 0;
@@ -92,6 +97,22 @@
 
   figure.small {
     height: max(20vh, 20vw);
+  }
+
+  aside {
+    color: var(--muted);
+    display: flex;
+    justify-content: space-between;
+  }
+
+  button {
+    border: none;
+    background: transparent;
+
+    &:hover,
+    &:focus {
+      color: var(--highlight);
+    }
   }
 
   @media (max-width: 1400px) {
--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -24,7 +24,7 @@
 
 <style lang="scss">
   figure {
-    height: 66vh;
+    height: 50vh;
 
     display: flex;
     align-items: center;
