badd5688c8ef17c3f53ddee19b6e54e4de4768f1
Patrick Mulholland <pm@laureat.ca>
2021-11-23 10:36:05 -0500

    Temp fix card background

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -51,8 +51,8 @@
 <style lang="scss">
   article {
     height: 100%;
-    //background: var(--darkish);
-    background: linear-gradient(180deg, transparent, var(--darkish));
+    background: var(--darkish);
+    //background: linear-gradient(180deg, transparent, var(--darkish));
     padding: calc(var(--gutter) / 2);
     border-radius: var(--corner);
 
