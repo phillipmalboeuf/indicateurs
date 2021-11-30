f753065018f356e7a025bbd4072056bee9a89754
Phillip Malboeuf <phil@phils.computer>
2021-11-29 20:06:52 -0500

    auth

--- src/hooks.ts
+++ src/hooks.ts
@@ -3,6 +3,26 @@ export async function handle({ request, resolve }) {
 
 	const response = await resolve(request)
 
+	if (!request.headers.authorization) {
+		return {
+			status: 401,
+			headers: {
+				'WWW-Authenticate': 'Basic'
+			}
+		}
+	}
+
+	const [user, password] = Buffer.from(request.headers.authorization.split(' ')[1], 'base64').toString().split(':')
+
+	if (user !== 'indicateurs' || password !== import.meta.env.VITE_PASSWORD) {
+		return {
+			status: 401,
+			headers: {
+				'WWW-Authenticate': 'Basic'
+			}
+		}
+	}
+
 	return {
 		...response,
 		headers: {
