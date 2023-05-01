4e20f99d55aa0b40a06e49a53fa54f12fb86d383
Phillip Malboeuf <phil@phils.computer>
2023-05-01 14:18:02 -0400

    no contenu

--- src/routes/[...locale]/index.svelte
+++ src/routes/[...locale]/index.svelte
@@ -24,6 +24,10 @@
   export let indicateurs: Entry<Indicateur>[]
   export let page: Entry<PageDocument>
 
+  $: {
+    page.fields.contenu = undefined
+  }
+
   let checked: string[] = $query.query.get('categories')?.split(',') || []
 </script>
 
