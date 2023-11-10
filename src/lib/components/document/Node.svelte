bf40226249cc149adc0ffd84c97752c597381fdf
Phillip Malboeuf <phil@phils.computer>
2023-11-10 12:23:51 -0500

    table styles

--- src/lib/components/document/Node.svelte
+++ src/lib/components/document/Node.svelte
@@ -32,9 +32,54 @@
     {#each node.content as item}<li>{#each item.content as node}<svelte:self node={node} />{/each}</li>{/each}
   </ol>
 
+{:else if node.nodeType === 'table'}
+  <table>
+    {#each node.content as item}<tr>{#each item.content as node}<svelte:self node={node} />{/each}</tr>{/each}
+  </table>
+{:else if node.nodeType === 'table-header-cell'}
+  <th>{#each node.content as item}<svelte:self node={item} />{/each}</th>
+{:else if node.nodeType === 'table-cell'}
+  <td>{#each node.content as item}<svelte:self node={item} />{/each}</td>
+
 {:else if node.nodeType === 'blockquote'}
   <blockquote>{#each node.content as code}<svelte:self node={code} />{/each}</blockquote>
 
 {:else if node.nodeType === 'embedded-asset-block'}
 <Picture media={node.data.target} />
 {/if}
+
+<style lang="scss">
+  table {
+    background: var(--dark);
+    border: 1px solid;
+    border-radius: 1em;
+    -webkit-border-horizontal-spacing: 0;
+    -webkit-border-vertical-spacing: 0;
+  }
+
+    td,
+    th {
+      // width: 50%;
+      vertical-align: top;
+      padding: 1em;
+
+      border-right: 1px solid;
+      border-bottom: 1px solid;
+
+      &:last-child {
+        border-right: none;
+      }
+
+      > *:last-child {
+        margin-bottom: 0;
+      }
+    }
+
+    th {
+      text-align: left;
+      font-weight: normal;
+      color: var(--dark);
+      background: var(--light);
+      padding: 0.5em 1em;
+    }
+</style>
