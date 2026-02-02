e1c4ce10b0ce78c0efdbeb2d553f253f7ea63674
Phillip Malboeuf <phil@phils.computer>
2026-02-02 10:37:34 -0500

    Revert "drizzle"
    This reverts commit 61a68725f12ab998a7a4aae747017102fde87977.

--- src/lib/clients/contentful.ts
+++ src/lib/clients/contentful.ts
@@ -2,7 +2,6 @@ import { PREVIEW } from '$env/static/private'
 import { getLocale } from '$lib/paraglide/runtime'
 import type { EntrySkeletonType } from 'contentful'
 import { createClient } from 'contentful'
-import { entries as entriesDb, entry as entryDb } from './contentful-db'
 
 const preview = PREVIEW === 'true'
 
@@ -13,15 +12,9 @@ export const content = createClient({
 }).withoutUnresolvableLinks
 
 export async function entry<T extends EntrySkeletonType>(id: string, query: {[key: string]: any}={}, include:1|2|3|4=2) {
-  if (!preview) {
-    return entryDb<T>(id, query, include)
-  }
   return (await content.getEntry<T>(id, { include, locale: {'en': 'en-CA'}[getLocale()], ...query }))
 }
 
 export async function entries<T extends EntrySkeletonType>(content_type: string, query: {[key: string]: any}={}, include:1|2|3|4=2) {
-  if (!preview) {
-    return entriesDb<T>(content_type, query, include)
-  }
   return (await content.getEntries<T>({ content_type, include, locale: {'en': 'en-CA'}[getLocale()], limit: 200, ...query })).items
 }
